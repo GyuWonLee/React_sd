4aaa2a3b39ac5da16fe6c021357893eb25e86557
GYUWON <37896874+GyuWonLee@users.noreply.github.com>
2024-01-22 10:26:42 +0900

    Add files via upload

--- 1.js
+++ 1.js
@@ -10,12 +10,35 @@ function isPositive(number) {
   }, 2000);
 }
 
-isPositive(
-  10,
-  (res) => {
-    console.log("성공적으로 수행됨 : ", res);
-  },
-  (err) => {
-    console.log("실패하였음 : ", err);
-  },
-);
+function isPositiveP(number) {
+  const executor = (resolve, reject) => {
+    // 실행자
+    setTimeout(() => {
+      if (typeof number === "number") {
+        // 성공 -> resolve
+        console.log(number);
+        resolve(number >= 0 ? "양수" : "음수");
+      } else {
+        // 실패 -> reject
+        reject("주어진 값이 숫자형 값이 아닙니다.");
+      }
+    }, 2000);
+  };
+
+  const asyncTask = new Promise(executor);
+  return asyncTask;
+}
+
+  const res = isPositive(101);
+
+  res.then(()=>{}).catch(()=>{})
+
+// isPositive(
+//   10,
+//   (res) => {
+//     console.log("성공적으로 수행됨 : ", res);
+//   },
+//   (err) => {
+//     console.log("실패하였음 : ", err);
+//   },
+// );
